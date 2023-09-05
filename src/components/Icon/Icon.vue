1d869af2666a735bfc482e30f41a38b9ca035fbf
18278399860 <867525213@qq.com>
2023-09-05 17:05:40 +0800

    pref:ts定义omit函数

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -11,7 +11,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { omit } from 'lodash-es'
+// import { omit } from 'lodash-es'
 import type { IconProps } from './types'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 defineOptions({
@@ -19,6 +19,16 @@ defineOptions({
     inheritAttrs: false
 })
 const props = defineProps<IconProps>()
+
+const omit = <T extends object, K extends keyof T>(object: T, paths: K[]): Omit<T, K> => {
+    const result = {} as any;
+    for (const key in object) {
+        if (!paths.includes(key as any)) {
+            result[key] = object[key];
+        }
+    }
+    return result;
+};
 
 const fontawesomeProps = computed(()=> omit(props, ['type', 'color']));
 
