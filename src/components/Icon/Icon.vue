eea25f65cd20ec60728c4349b9fff06da06213f8
18278399860 <867525213@qq.com>
2023-09-05 09:23:13 +0800

    feat:图表组件

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <i 
+        class="c-icon" 
+        :class="{[`c-icon--${type}`]:type}"
+        :style="curstomStyle"
+        v-bind="$attrs"
+    >
+        <font-awesome-icon v-bind="fontawesomeProps" />
+    </i>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { omit } from 'lodash-es'
+import type { IconProps } from './types'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+defineOptions({
+    name: 'CIcon',
+    inheritAttrs: false
+})
+const props = defineProps<IconProps>()
+
+const fontawesomeProps = computed(()=> omit(props, ['type', 'color']));
+
+const curstomStyle = computed(()=> props.color ? { color: props.color }:{ })
+
+</script>
+
+<style scoped>
+
+</style>
