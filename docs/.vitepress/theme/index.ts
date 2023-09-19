29c39369a1fd432a8f1c8e3aab9f17d9b90dae3e
18278399860 <867525213@qq.com>
2023-09-19 08:45:23 +0800

    feat: vitepress文档

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,15 @@
+import DefaultTheme from 'vitepress/theme'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import '../../../src/styles/index.css'
+import { AntDesignContainer, ElementPlusContainer, NaiveUIContainer } from '@vitepress-demo-preview/component'
+import '@vitepress-demo-preview/component/dist/style.css'
+
+library.add(fas)
+
+export default {
+    ...DefaultTheme,
+    enhanceApp({ app }) {
+      app.component('demo-preview', AntDesignContainer)
+    }
+  }
---
+++ docs/examples/button/basic.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mt-4">
+        <Button>hello</Button>
+        <Button plain>plain</Button>
+        <Button round>round</Button>
+        <Button circle>circle</Button>
+        <Button disabled>disabled</Button>
+    </div>
+    <div class="mt-4"> 
+        <Button type="primary">primary</Button>
+        <Button type="success">primasuccessry</Button>
+        <Button type="info">info</Button>
+        <Button type="danger">danger</Button>
+        <Button type="warning">warning</Button>
+    </div>
+    <div class="mt-4">
+        <Button type="primary" plain>primary</Button>
+        <Button type="success" plain>primasuccessry</Button>
+        <Button type="info" plain>info</Button>
+        <Button type="danger" plain>danger</Button>
+        <Button type="warning" plain>warning</Button>
+    </div>
+    <div class="mt-4">
+        <Button type="primary" loading >warning</Button>
+        <Button type="primary"  icon="fa-solid fa-user-secret" >warning</Button>        
+    </div>        
+
+
+  
+
+</template>
+
+<script setup>
+import Button from "../../../src/components/Button/Button.vue"
+
+</script>
