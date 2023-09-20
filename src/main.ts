e7dffd265ac46b996a1cc5a14df9f0c3076602ec
18278399860 <867525213@qq.com>
2023-09-20 11:28:01 +0800

    pref:replace postcss with sass

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './styles/index.css'
+import './styles/index.scss'
 
 // fontawesome 图标库
 import { library } from '@fortawesome/fontawesome-svg-core'
